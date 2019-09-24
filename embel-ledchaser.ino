478d49246a50c43f18a2edef625e277b29414178
guilyx <erwin.lejeune15@gmail.com>
2019-09-24 18:06:48 +0200

    :construction: Begin code segmentation

---
+++ embel-ledchaser.ino
@@ -0,0 +1,21 @@
+#include "ledchaser.h"
+
+
+bool gMode = false;
+
+
+void setup() {
+    if (!gMode) { //lcd incr
+        setLCD();
+    } else { // motor/encoders
+        setupChaser();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    }
+}
+
+void isr() {
+    if (gMode) irqHelper();
+}
+
+void loop() {
+    if (!gMode) loopLCD();
+}
